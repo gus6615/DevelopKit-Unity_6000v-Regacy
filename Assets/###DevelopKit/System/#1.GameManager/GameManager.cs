ed5aeb52dbe92f5eb10784388c2aa7338c558284
Cheonnyang <wns6615@gmail.com>
2024-04-15 21:26:34 +0900

    feat: 싱글톤 디자인패턴 구현

--- Assets/###DevelopKit/System/#1.GameManager/GameManager.cs
+++ Assets/###DevelopKit/System/#1.GameManager/GameManager.cs
@@ -2,40 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    private static T instance;
-    public static T Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = GameObject.FindObjectOfType<T>();
-                if (instance == null)
-                {
-                    var newObj = new GameObject(typeof(T).Name);
-                    instance = newObj.AddComponent<T>();
-                }
-            }
-            return instance;
-        }
-    }
-
-    public virtual void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this as T;
-            DontDestroyOnLoad(instance.gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-}
-
 public class GameManager : Singleton<GameManager>
 {
     
