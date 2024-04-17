4684cb3f495adeba8c571a5adad2e9a0ce57f63c
Cheonnyang <wns6615@gmail.com>
2024-04-17 15:33:01 +0900

    feat: Debug 최적화 추가

--- Assets/###DevelopKit/System/#1.GameManager/GameManager.cs
+++ Assets/###DevelopKit/System/#1.GameManager/GameManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Logger = NKStudio.Logger;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    private void Start()
+    {
+        Logger.Log("GameManager Start");
+    }
 }
